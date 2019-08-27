d63e852421293d4fb3d5fc475c009313ce1ddefe
pigzaza <zhaozhe1985@163.com>
2019-08-27 20:17:45 +0800

    简化了程序，全部装入函数里；增加了定时功能，日期改为了当前时间now。

---
+++ HowMuchIsSelling/test1+regex+def.py
@@ -0,0 +1,41 @@
+import requests
+import re
+import schedule #pip
+import time
+import csv      #pip
+import datetime
+
+
+def Get_info():
+
+    url = 'https://bj.lianjia.com'
+    headers = {'user-agent': 'my-app/0.0.1'}
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        r.encoding = r.apparent_encoding        #转换字符编码??
+        html = r.text
+        res = re.findall(r"[\u4e00-\u9fa5]\s+(\d{4,6})\s+[\u4e00-\u9fa5]",html)
+        num = res[0]
+
+    return num
+
+def add_data(num):
+    today=datetime.date.today()
+
+    now = datetime.datetime.now()
+    f = open('lj.csv','a+',encoding='utf-8')
+    csv_writer = csv.writer(f)
+    csv_writer.writerow([now,num])
+    f.close()
+
+def job():
+    num = Get_info()
+
+    add_data(num)
+
+schedule.every(1).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
